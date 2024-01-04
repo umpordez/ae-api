d7d38805791d4734e6052acb36e5e78dd752e295
Deividy Metheler Zachetti <deividyz@gmail.com>
2024-01-04 06:17:27 -0300

    first commit!

---
+++ ae-api.js
@@ -0,0 +1,191 @@
+const axios = require('axios');
+const querystring = require('querystring');
+const V = require('argument-validator');
+
+const crypto = require('crypto');
+
+// https://www.yuque.com/richardyanhao-mekyj/cm0dv1/kuprl9yt3ggokqem
+
+const configByEnv = {
+    dev: {
+        apiBaseUrl: 'https://api-sg.aliexpress.com',
+        authorizeBaseUrl: 'https://api-sg.aliexpress.com/oauth/authorize'
+    },
+
+    prod: {
+        apiBaseUrl: 'https://api-sg.aliexpress.com',
+        authorizeBaseUrl: 'https://api-sg.aliexpress.com/oauth/authorize'
+    }
+};
+
+function getHmacsha256Hex(secret, text) {
+    return crypto.createHmac('sha256', secret).update(text).digest('hex');
+}
+
+// eslint-disable-next-line max-len
+// https://open.aliexpress.com/doc/doc.htm?spm=a2o9m.11193531.0.0.41524bd639wEKx&nodeId=27493&docId=118729#/?docId=742
+
+function getSignatureByParams(appSecret, pathname, params) {
+    const parts = [ pathname ];
+    const keys = Object.keys(params);
+
+    keys.sort((a, b) => a.localeCompare(b));
+
+    for (const key of keys) {
+        parts.push(`${key}${params[key]}`);
+    }
+
+    const str = parts.join('');
+
+    return getHmacsha256Hex(appSecret, str).toUpperCase();
+}
+
+
+class AliexpressClient {
+    constructor(env, appKey, appSecret, accessToken) {
+        V.string(env, 'token');
+        V.string(appKey, 'appKey');
+        V.string(appSecret, 'appSecret');
+
+        this.env = env;
+        this.appKey = appKey;
+        this.appSecret = appSecret;
+        this.accessToken = accessToken;
+
+        this.config = configByEnv[env];
+
+        this.lastRequests = [];
+        this.lastResponses = [];
+    }
+
+    getSignatureByParams(pathname, body) {
+        return getSignatureByParams(
+            this.appSecret,
+            pathname !== '/sync' ? pathname : '',
+            body
+        );
+    }
+
+    async doRequest(pathname, body = {}) {
+        V.string(pathname, 'pathname');
+
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        };
+
+        let url = this.config.apiBaseUrl;
+
+        if (pathname.startsWith('/auth')) {
+            url += `/rest`;
+        }
+
+        url += pathname;
+
+        body.app_key = this.appKey;
+        body.sign_method = 'sha256';
+        body.timestamp = new Date().getTime();
+
+        body.target_language = this.defaultLanguage;
+        body.ship_to_country = this.defaultShipCountry;
+        body.target_currency = this.defaultCurrency;
+
+        const signature = this.getSignatureByParams(
+            pathname !== '/sync' ? pathname : '',
+            body
+        );
+
+        const data = querystring.stringify({
+            ...body,
+            sign: signature
+        });
+
+        const options = { url, data, method: 'POST', headers };
+        this.lastRequest = { ...options };
+
+        let response;
+        const { stack } = new Error();
+
+        try {
+            const res = await axios(options);
+
+            this.lastResponse = { body: res.data, headers: res.headers };
+            this.lastResponses.push(this.lastResponse);
+
+            response = res.data;
+
+            if (response.error_response) {
+                response = response.error_response;
+                response.message = response.message || response.msg;
+            }
+
+            if (response.code && response.message && response.request_id) {
+                const err = new Error(`${response.code} - ${response.message}`);
+
+                err.code = response.code;
+                err.type = response.type;
+                err.request_id = response.request_id;
+                err.rawResponse = response;
+
+                throw err;
+            }
+        } catch (ex) {
+            ex.response = ex.response || ex.res;
+            if (ex.response) {
+                const { response } = ex;
+                this.lastResponse = {
+                    body: response.data,
+                    headers: response.headers
+                };
+
+                this.lastResponses.push(this.lastResponse);
+
+                if (response.data && response.data.error_message) {
+                    ex.message = response.data.error_message;
+                } else if (response.data) {
+                    ex.message += `\n${response.data}`;
+                }
+            }
+
+            ex.stack = stack;
+            throw ex;
+        }
+
+        return response;
+    }
+
+    doAuthenticateRequest(method, body = {}) {
+        body.method = method;
+        body.access_token = this.accessToken;
+
+        V.string(body.access_token, 'access_token');
+        V.string(body.method, 'method');
+
+        return this.doRequest('/sync', body);
+    }
+
+    getAcessTokenByCode(code) {
+        V.string(code, 'code');
+
+        return this.doRequest('/auth/token/create', { code });
+    }
+
+    getAccessTokenByRefreshToken(refreshToken) {
+        V.string(refreshToken, 'refreshToken');
+
+        return this.doRequest('/auth/token/refresh', {
+            refresh_token: refreshToken
+        });
+    }
+
+    getAuthorizeUrl(redirectUri) {
+        return `${this.config.apiBaseUrl}/oauth/authorize?${querystring
+            .stringify({
+                response_type: 'code',
+                force_auth: true,
+                redirect_uri: redirectUri,
+                client_id: this.appKey
+            })}`;
+    }
+}
+
+module.exports = AliexpressClient;
---
+++ tests/ae-api/client.test.js
@@ -0,0 +1,127 @@
+require('../test-helper');
+
+const assert = require('assert');
+const AeClient = require('../../ae-api');
+
+describe('Aliexpress Client', () => {
+    const {
+        TEST_TOKEN,
+        TEST_APP_KEY,
+        TEST_APP_SECRET,
+        TEST_CODE,
+        TEST_REFRESH_TOKEN
+    } = process.env;
+
+    const appKey = '123456';
+    const appSecret = 'helloworld';
+
+    it('Test authorize URL' , () => {
+        const client = new AeClient('dev', appKey, appSecret);
+        const url = client.getAuthorizeUrl(
+            'https://api.fornece.club/webhook/aliexpress'
+        );
+
+        assert.strictEqual(
+            url,
+            // eslint-disable-next-line max-len
+            'https://api-sg.aliexpress.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri=https%3A%2F%2Fapi.fornece.club%2Fwebhook%2Faliexpress&client_id=123456'
+        );
+    });
+
+    it('Test get signature', () => {
+        const client = new AeClient('dev', appKey, appSecret);
+        const signature1 = client.getSignatureByParams(
+            '',
+            {
+                access_token: 'test',
+                app_key: '123456',
+                timestamp: '1517820392000',
+                sign_method: 'sha256',
+                aliexpress_category_id: '200135143',
+                method: 'aliexpress.logistics.redefining.getonlinelogisticsinfo',
+            }
+        );
+
+        assert.strictEqual(
+            signature1,
+            'F7F7926B67316C9D1E8E15F7E66940ED3059B1638C497D77973F30046EFB5BBB'
+        );
+
+        const signature2 = client.getSignatureByParams(
+            '/auth/token/create',
+            {
+                app_key: '12345678',
+                code: '3_500102_JxZ05Ux3cnnSSUm6dCxYg6Q26',
+                timestamp: '1517820392000',
+                sign_method: 'sha256'
+            }
+        );
+
+        assert.strictEqual(
+            signature2,
+            '35607762342831B6A417A0DED84B79C05FEFBF116969C48AD6DC00279A9F4D81'
+
+        );
+    });
+
+    it.skip('Generate access token', async () => {
+        const client = new AeClient('dev', TEST_APP_KEY, TEST_APP_SECRET);
+        const token = await client.getAcessTokenByCode(TEST_CODE);
+
+        /*
+            {
+                refresh_token_valid_time: 1704531678000,
+                havana_id: '',
+                expire_time: 1704445278000,
+                locale: 'zh_CN',
+                user_nick: 'br1043680154',
+                access_token: '',
+                refresh_token: '',
+                user_id: '163688471',
+                account_platform: 'buyerApp',
+                refresh_expires_in: 172800,
+                expires_in: 86400,
+                sp: 'ae',
+                seller_id: '',
+                account: '',
+                code: '0',
+                request_id: ''
+            }
+        */
+
+        assert(token);
+    });
+
+    it.skip('Generate a new token by refresh token', async () => {
+        const client = new AeClient('dev', TEST_APP_KEY, TEST_APP_SECRET);
+        const token = await client.getAccessTokenByRefreshToken(
+            TEST_REFRESH_TOKEN
+        );
+
+        assert(token);
+    });
+
+    it.skip('getProduct', async () => {
+        const client = new AeClient(
+            'dev',
+            TEST_APP_KEY,
+            TEST_APP_SECRET,
+            TEST_TOKEN
+        );
+
+        client.defaultLanguage = 'pt';
+        client.defaultShipCountry = 'BR';
+        client.defaultCurrency = 'BRL';
+
+        const response = await client.doAuthenticateRequest(
+            'aliexpress.ds.product.get',
+            { product_id: '1005006057059635' }
+        );
+
+        assert(response);
+        const res = response.aliexpress_ds_product_get_response;
+
+        assert(res.rsp_msg === 'Call succeeds');
+        assert(res.rsp_code === 200);
+    });
+});
